84fb09ffbb29f52b6083bad357d4fd801d19c3b1
pieyyulr <128105743+pieyyulr@users.noreply.github.com>
2023-03-24 12:49:35 +0800

    added btn_animation

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function clickButton()
 {
     var audioSound = this.innerHTML;
     keyboardSound(audioSound);
-    console.log(key);
+    keyboardAnimation(audioSound);
+    // console.log(key);
    
 }};
 
@@ -18,6 +19,7 @@ document.addEventListener("keydown", keyBoardEvent);
 function keyBoardEvent(event)
 {
     keyboardSound(event.key);
+    keyboardAnimation(event.key);
 }
 
 function keyboardSound(key)
@@ -26,43 +28,43 @@ function keyboardSound(key)
     case "p":
         var zupGuys = new Audio("audio/zup guys.mp3");
         zupGuys.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
     
     case "o":
         var ohYeah = new Audio("audio/huh.oh yeah.wav");
         ohYeah.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
-
+    
     case "r":
         var shoutOut = new Audio("audio/shout out.mp3");
         shoutOut.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
     
     case "t":
         var lalala = new Audio("audio/lalalalalala.mp3");
         lalala.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
 
     case "f":
         var aha = new Audio("audio/aha.mp3");
         aha.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
 
     case "l":
         var playSound = new Audio("audio/play sound_ren.wav");
         playSound.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
 
     case "i":
         var broom = new Audio("audio/broom.mp3");
         broom.play();
-        this.style.color ="red";
+        // this.style.color ="red";
         break;
 
     default: 
@@ -71,10 +73,16 @@ function keyboardSound(key)
     }
 }
 
+function keyboardAnimation(currentKey)
+{
+    var pressedKey = document.querySelector("." + currentKey);
+    console.log(pressedKey);
+    pressedKey.classList.add("whenPress");
+    setTimeout(function(){
+    pressedKey.classList.remove("whenPress");
+    }, 200);
+}
 
 
-// this.style.color ="red";
-//     // // 
-//     // // // alert("clicked!");
-//     // // 
-//     // // console.log(this.innerHTML);
+
+
