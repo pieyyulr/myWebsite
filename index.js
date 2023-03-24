ad1d447f0fa2945ab6f70568421379b82ec34781
pieyyulr <128105743+pieyyulr@users.noreply.github.com>
2023-03-24 12:58:52 +0800

    update case

--- index.js
+++ index.js
@@ -28,43 +28,36 @@ function keyboardSound(key)
     case "p":
         var zupGuys = new Audio("audio/zup guys.mp3");
         zupGuys.play();
-        // this.style.color ="red";
         break;
     
     case "o":
         var ohYeah = new Audio("audio/huh.oh yeah.wav");
         ohYeah.play();
-        // this.style.color ="red";
         break;
     
     case "r":
         var shoutOut = new Audio("audio/shout out.mp3");
         shoutOut.play();
-        // this.style.color ="red";
         break;
     
     case "t":
         var lalala = new Audio("audio/lalalalalala.mp3");
         lalala.play();
-        // this.style.color ="red";
         break;
 
     case "f":
         var aha = new Audio("audio/aha.mp3");
         aha.play();
-        // this.style.color ="red";
         break;
 
     case "l":
         var playSound = new Audio("audio/play sound_ren.wav");
         playSound.play();
-        // this.style.color ="red";
         break;
 
     case "i":
         var broom = new Audio("audio/broom.mp3");
         broom.play();
-        // this.style.color ="red";
         break;
 
     default: 
